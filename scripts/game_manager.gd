85d8ce2a2c3d6ea080fa7ade4afc7735ee8f05d5
Michael <mirima92@gmail.com>
2024-11-28 16:50:19 -0500

    Create pitfall trap and reset the level if the player falls in

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,3 +8,8 @@ func add_point():
 	points += 1
 	print(points)
 	points_label.text = "Points: " + str(points)
+
+
+func _on_fall_area_body_entered(body: Node2D) -> void:
+	if(body.name == "Player"):
+		get_tree().reload_current_scene()
